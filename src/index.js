c4658f80680b0a9dd791048ac88660509a7e058b
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-04-17 23:10:24 +0530

    Uploading to Github

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './Resources/fonts/walsheim/GTWalsheimPro-Medium.ttf';
+import './Resources/fonts/walsheim/GTWalsheimPro-Regular.ttf';
+import './Resources/fonts/walsheim/GTWalsheimPro-Light.ttf';
+import './Resources/fonts/walsheim/GTWalsheimPro-Bold.ttf';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+reportWebVitals();
