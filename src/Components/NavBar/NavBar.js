c6ac8c97681ab5779768c81d94b4dcf42cc64ce4
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-04-17 23:31:04 +0530

    update favicon and Title

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -117,7 +117,7 @@ const NavBar = props => {
               onMouseLeave={handleHover}
             >
                 <GitHub className={styles.gh} />
-                <h3>gianlucajahn</h3>
+                <h3>vaibhavc2</h3>
             </div>
 
             <div 
